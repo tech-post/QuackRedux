a20493cdfdc49d3e1e07ef8903f64fbddfb93f17
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-31 10:59:43 -0400

    created single post component

---
+++ client/src/components/layout/Post.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Post extends Component {
+
+  componentDidMount = () => {
+    
+  }
+
+  render(){
+    
+    return(
+      <div id='post'>
+        <h1>This is a single post</h1>
+      </div>
+    )
+  }
+}
+
+export default Post;
